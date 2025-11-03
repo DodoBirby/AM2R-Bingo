36371eeabd9a84274c127390a97cd368894a45c4
DodoBirb <95009974+DodoBirby@users.noreply.github.com>
2025-11-03 18:05:09 +1100

    Add BingoSyncClient

---
+++ BingoSyncIntegration/BingoSyncClient.cs
@@ -0,0 +1,87 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace BingoSyncIntegration;
+
+public partial class BingoSyncClient : IDisposable
+{
+	const string BingoSyncUrl = "https://bingosync.com";
+	const string PasswordFieldName = "passphrase";
+	const string PlayerNameFieldName = "player_name";
+
+	readonly CookieContainer cookieContainer = new();
+	readonly HttpClientHandler httpHandler;
+	readonly HttpClient client;
+
+	static readonly JsonSerializerOptions options = new()
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+	};
+
+	public string ConnectedRoom { get; private set; } = string.Empty;
+
+	public BingoSyncClient()
+	{
+		httpHandler = new() { UseCookies = true, CookieContainer = cookieContainer };
+		client = new(httpHandler);
+	}
+
+	public async Task ConnectAsync(string roomId, string password)
+	{
+		var loginResponse = await client.GetAsync($"{BingoSyncUrl}/room/{roomId}");
+		loginResponse.EnsureSuccessStatusCode();
+
+		var loginScreenHtml = await loginResponse.Content.ReadAsStringAsync();
+
+		var formInputRegex = FormInputRegex();
+
+		var matches = formInputRegex.Matches(loginScreenHtml);
+		var formSubmitDict = new Dictionary<string, string>();
+
+		foreach (Match match in matches)
+		{
+			formSubmitDict.Add(match.Groups[1].Value, match.Groups[2].Value);
+		}
+		formSubmitDict.Add(PasswordFieldName, password);
+		formSubmitDict.Add(PlayerNameFieldName, "bot");
+
+		var content = new FormUrlEncodedContent(formSubmitDict);
+
+		var postResponse = await client.PostAsync($"{BingoSyncUrl}/room/{roomId}", content);
+		postResponse.EnsureSuccessStatusCode();
+
+		ConnectedRoom = roomId;
+		// Now cookie container contains the session id cookie which enables further post requests
+	}
+
+	public async Task SetColor(int slotNum, string color, bool removeColor)
+	{
+		if (string.IsNullOrEmpty(ConnectedRoom))
+		{
+			return;
+		}
+
+		var selectBody = new BingoSyncSelectJSON()
+		{
+			Color = color,
+			Slot = slotNum.ToString(),
+			RemoveColor = removeColor,
+			Room = ConnectedRoom
+		};
+
+		var putResponse = await client.PutAsJsonAsync($"{BingoSyncUrl}/api/select", selectBody, options);
+		putResponse.EnsureSuccessStatusCode();
+	}
+
+	public void Dispose()
+	{
+		client.Dispose();
+		GC.SuppressFinalize(this);
+	}
+
+	[GeneratedRegex("<input[^>]*name=\"([^\"]*)\"[^>]*value=\"([^\"]*)\"[^>]*>")]
+	private static partial Regex FormInputRegex();
+
+}
---
+++ BingoSyncIntegration/BingoSyncSelectJSON.cs
@@ -0,0 +1,12 @@
+﻿namespace BingoSyncIntegration;
+
+public class BingoSyncSelectJSON
+{
+	public string Room { get; set; } = string.Empty;
+
+	public bool RemoveColor { get; set; } = false;
+
+	public string Slot { get; set; } = "1";
+
+	public string Color { get; set; } = string.Empty;
+}
