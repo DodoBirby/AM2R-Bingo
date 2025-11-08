6d0b051a3bac1ae160456700212cad92cf98fef6
DodoBirb <95009974+DodoBirby@users.noreply.github.com>
2025-11-08 14:43:07 +1100

    Add InGame deserialisation

--- BingoSyncIntegration/AM2RDataJSON.cs
+++ BingoSyncIntegration/AM2RDataJSON.cs
@@ -1,14 +1,17 @@
-﻿namespace BingoSyncIntegration;
+﻿using System.Text.Json.Serialization;
+
+namespace BingoSyncIntegration;
 
 public class AM2RDataJSON
 {
+	[JsonConverter(typeof(StringBoolJSONConverter))]
 	public bool InGame { get; set; }
 
-	public required Dictionary<string, double> Metroids { get; set; }
+	public Dictionary<string, double>? Metroids { get; set; }
 
-	public required Dictionary<string, double> Items { get; set; }
+	public Dictionary<string, double>? Items { get; set; }
 
-	public required Dictionary<string, double> Events { get; set; }
+	public Dictionary<string, double>? Events { get; set; }
 
-	public required Dictionary<string, double> MapTiles { get; set; }
+	public Dictionary<string, double>? MapTiles { get; set; }
 }
---
+++ BingoSyncIntegration/StringBoolJSONConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BingoSyncIntegration;
+
+public class StringBoolJSONConverter : JsonConverter<bool>
+{
+	public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		var str = reader.GetString();
+		return str == "true";
+	}
+
+	public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+	{
+		throw new NotImplementedException();
+	}
+}
